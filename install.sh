2e4e5c4708379998bd34e4daf218dfc310f0014d
root <root@DESKTOP-U57L9VJ.localdomain>
2026-05-18 05:34:09 -0700

    install.sh fix bug4

--- install.sh
+++ install.sh
@@ -110,7 +110,7 @@ echo "[+] Installing google-chrome"
 wget -q -O chrome.deb \
 "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 
-sudo apt install ./chrome.deb
+sudo apt --fix-broken install ./chrome.deb
 sudo apt-get install -f
 
 
